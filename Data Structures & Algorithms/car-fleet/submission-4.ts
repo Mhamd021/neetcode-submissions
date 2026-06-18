f222791472d6df388119fded98d4bd95901dd190
Mhamd Ghanoum <108063365+Mhamd021@users.noreply.github.com>
2026-06-18 12:37:21 +0300

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target: number, position: number[], speed: number[]): number {
+        //define cars map : target - p / s which means we add the position and how many steps left to reach the target
+        // then we sort it 
+        // fleets and maxTime
+
+        const cars = position.map(
+            (p,i) => [
+                p,(target - p) / speed[i] 
+            ]
+        );
+        cars.sort((a,b)=> b[0] - a[0]);
+        let fleets =0;
+        let maxTime =0;
+        for(const [,time] of cars)
+        {
+            if(time > maxTime){
+                fleets++;
+                maxTime = time;
+            }
+        }
+        return fleets;
+    }
+}
