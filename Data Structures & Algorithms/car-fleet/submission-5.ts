f911f5aa953e5f6122a9e3179fabfde807583814
Mhamd Ghanoum <108063365+Mhamd021@users.noreply.github.com>
2026-06-19 17:50:36 +0300

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target: number, position: number[], speed: number[]): number {
+        
+
+        const cars = position.map(
+            (p,i) => [
+                p,(target - p) / speed[i] 
+            ]
+        );
+       cars.sort((a,b) => b[0]-a[0]);
+       let fleet = 0;
+       let maxTime =0;
+       for(const [,time] of cars)
+       {
+        if(time>maxTime)
+        {
+            fleet++;
+            maxTime = time;
+        }
+       }
+       return fleet;
+    }
+}
